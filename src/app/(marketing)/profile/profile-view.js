8f1f0ea7a190b237035cc1a0e508957e3d1a1150
codemine24 <codemine24@gmail.com>
2025-01-02 19:44:14 +0600

    profile basic view

--- src/app/(marketing)/profile/profile-view.js
+++ src/app/(marketing)/profile/profile-view.js
@@ -1,3 +1,19 @@
+import { Card } from '@mui/material';
+import Grid from '@mui/material/Grid2';
+
+import { LeftProfileOverview } from './_components/left-profile-overview';
+
 export const ProfileView = () => {
-    return <div>profile view</div>;
+  return (
+    <Grid container>
+      <Grid item size={{ xs: 12, md: 3 }}>
+        <Card>
+          <LeftProfileOverview />
+        </Card>
+      </Grid>
+      <Grid item size={{ xs: 12, md: 9 }}>
+        hello
+      </Grid>
+    </Grid>
+  );
 };
