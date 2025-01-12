f9348b43df963c13f1770d01b836df70b32f6467
codemine24 <codemine24@gmail.com>
2025-01-12 12:37:03 +0600

    campaign card modificaiton done in campaign list page.

--- src/app/(marketing)/campaign/_components/right-campaign-overview.js
+++ src/app/(marketing)/campaign/_components/right-campaign-overview.js
@@ -1,7 +1,9 @@
 import Link from 'next/link';
-import { Box, Paper, Typography } from '@mui/material';
-import Grid from '@mui/material/Grid2';
 import { textShortner } from '@/utils/utils';
+import { Box, Chip, Paper, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid2';
+import { Iconify } from '@/components/iconify/iconify';
+import dayjs from 'dayjs';
 
 export const RightCampaignOverview = ({ data }) => {
   return (
@@ -9,7 +11,7 @@ export const RightCampaignOverview = ({ data }) => {
       <Grid container spacing={2}>
         {data.map((item, index) => (
           <Grid item size={{ xs: 12, md: 4 }} key={index}>
-            <SingleCard data={item} />
+            <CampaignCard data={item} />
           </Grid>
         ))}
       </Grid>
@@ -17,33 +19,52 @@ export const RightCampaignOverview = ({ data }) => {
   );
 };
 
-const SingleCard = ({ data }) => {
+const CampaignCard = ({ data }) => {
   return (
-    <Box>
+    <Box sx={{ position: 'relative' }}>
       <Paper elevation={1} variant="outlined">
         <Box
           component="img"
           src={`${process.env.NEXT_PUBLIC_SUPABASE_PREVIEW_PREFIX}${data.thumbnail}`}
           sx={{ height: 200, width: '100%', objectFit: 'cover', border: 0, borderRadius: '5px 5px 0 0' }}
         />
+        <Chip size="small" color="secondary" label={data.status} sx={{ position: 'absolute', top: 10, left: 10 }} />
         <Box p={2}>
           <Typography color="text.secondary" sx={{ fontWeight: 600 }}>
             {data.name}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            {textShortner(data.description, 80)}
+            {textShortner(data.description || '-', 80)}
           </Typography>
-          <Typography variant="body2" color="text.secondary" fontWeight={600}>
-            Model: {data.model || '-'}
-          </Typography>
+          <Box display="flex" alignItems="center" sx={{ mt: 1 }}>
+            <Iconify icon="material-symbols:calendar-month" width={14} height={14} sx={{ mr: 1 }} />
+            <Typography variant="body2" color="text.secondary">
+              Start Date: {dayjs(data.start_date).format('MMM D, YYYY')}
+            </Typography>
+          </Box>
+          <Box display="flex" alignItems="center" sx={{ mb: 1 }}>
+            <Iconify icon="material-symbols:calendar-month" width={14} height={14} sx={{ mr: 1 }} />
+            <Typography variant="body2" color="text.secondary">
+              End Date: {dayjs(data.end_date).format('MMM D, YYYY')}
+            </Typography>
+          </Box>
           <Link
-            href={`campaign/${data.slug}`}
-            style={{
-              fontSize: '0.9rem',
-              color: 'var(--mui-palette-text-secondary)',
-            }}
+            href={`/campaign/${data.slug}`} // Relative path to the campaign
+            passHref
           >
-            View Campaign
+            <Typography
+              variant="body2"
+              sx={{
+                fontSize: '0.9rem',
+                color: 'var(--mui-palette-text-secondary)',
+                textDecoration: 'none',
+                '&:hover': {
+                  textDecoration: 'underline',
+                },
+              }}
+            >
+              View Campaign
+            </Typography>
           </Link>
         </Box>
       </Paper>
