1ff3824535210b45ba944dceb3269ae604b925c9
codemine24 <codemine24@gmail.com>
2025-01-25 22:01:51 +0600

    list view basic setup done

--- src/app/(marketing)/portfolio/_components/portfolio-listview.js
+++ src/app/(marketing)/portfolio/_components/portfolio-listview.js
@@ -6,11 +6,13 @@ import { PageContainer } from '@/components/container/PageContainer';
 import { RefreshPlugin } from '@/components/core/plugins/RefreshPlugin';
 import { EditableDataTable } from '@/components/data-table/editable-data-table';
 import { DeleteConfirmationPopover } from '@/components/dialog/delete-confirmation-popover';
+import { Iconify } from '@/components/iconify/iconify';
+import { dateFormatter } from '@/utils/date-formatter';
+import { IconButton } from '@mui/material';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import { Plus as PlusIcon } from '@phosphor-icons/react/dist/ssr/Plus';
-import moment from 'moment';
 
 import {
   createPortfolioAsync,
@@ -19,52 +21,64 @@ import {
   updatePortfolioAsync,
 } from '../_lib/portfolio.actions';
 import { defaultPortfolio } from '../_lib/portfolio.types';
-
-// table columns
-const columns = [
-  { field: 'project_title', headerName: 'Project Title', width: 280, editable: true },
-  { field: 'category', headerName: 'Category', width: 150, editable: true },
-  { field: 'video_url', headerName: 'Video URL', width: 200, editable: true },
-  { field: 'hero_image', headerName: 'Hero Image', width: 150, editable: true },
-  { field: 'field_image', headerName: 'Field Image', width: 150, editable: true },
-  { field: 'thumbnail', headerName: 'Thumbnail', width: 150, editable: true },
-  { field: 'vertical_gallery_images', headerName: 'Vertical Gallery Images', width: 200, editable: true },
-  { field: 'horizontal_gallery_images', headerName: 'Horizontal Gallery Images', width: 200, editable: true },
-  {
-    field: 'date',
-    headerName: 'Date',
-    width: 150,
-    editable: true,
-    valueGetter: (value, row) => moment(value).format('DD-MM-YYYY HH:mm:ss'),
-  },
-  { field: 'short_description', headerName: 'Short Description', width: 200, editable: true },
-  { field: 'full_description', headerName: 'Full Description', width: 300, editable: true },
-  { field: 'state', headerName: 'State', width: 150, editable: true },
-  { field: 'partner_hq', headerName: 'Partner HQ', width: 150, editable: true },
-  { field: 'user_id', headerName: 'User ID', width: 150, editable: true },
-  {
-    field: 'created_at',
-    headerName: 'Created At',
-    width: 180,
-    editable: true,
-    valueGetter: (value, row) => moment(value).format('DD-MM-YYYY HH:mm:ss'),
-  },
-  {
-    field: 'updated_at',
-    headerName: 'Updated At',
-    width: 180,
-    editable: true,
-    valueGetter: (value, row) => moment(value).format('DD-MM-YYYY HH:mm:ss'),
-  },
-];
+import { ManagePortfolioRightPanel } from './manage-portfolio-right-panel';
 
 export const PortfolioListView = () => {
+  // table columns
+  const columns = [
+    {
+      field: 'actions',
+      headerName: 'Actions',
+      width: 70,
+      editable: false,
+      renderCell: (params) => (
+        <IconButton onClick={() => handleEdit(params.row)}>
+          <Iconify icon="ci:expand" />
+        </IconButton>
+      ),
+    },
+    { field: 'project_title', headerName: 'Project Title', width: 280, editable: true },
+    { field: 'category', headerName: 'Category', width: 150, editable: true },
+    { field: 'video_url', headerName: 'Video URL', width: 200, editable: true },
+    { field: 'hero_image', headerName: 'Hero Image', width: 150, editable: true },
+    { field: 'field_image', headerName: 'Field Image', width: 150, editable: true },
+    { field: 'thumbnail', headerName: 'Thumbnail', width: 150, editable: true },
+    { field: 'vertical_gallery_images', headerName: 'Vertical Gallery Images', width: 200, editable: true },
+    { field: 'horizontal_gallery_images', headerName: 'Horizontal Gallery Images', width: 200, editable: true },
+    {
+      field: 'date',
+      headerName: 'Date',
+      width: 150,
+      editable: true,
+      valueGetter: (value, row) => dateFormatter(value),
+    },
+    { field: 'short_description', headerName: 'Short Description', width: 200, editable: true },
+    { field: 'full_description', headerName: 'Full Description', width: 300, editable: true },
+    { field: 'state', headerName: 'State', width: 150, editable: true },
+    { field: 'partner_hq', headerName: 'Partner HQ', width: 150, editable: true },
+    { field: 'user_id', headerName: 'User ID', width: 150, editable: true },
+    {
+      field: 'created_at',
+      headerName: 'Created At',
+      width: 180,
+      editable: true,
+      valueGetter: (value, row) => dateFormatter(value),
+    },
+    {
+      field: 'updated_at',
+      headerName: 'Updated At',
+      width: 180,
+      editable: true,
+      valueGetter: (value, row) => dateFormatter(value),
+    },
+  ];
   const [records, setRecords] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
   const [pagination, setPagination] = React.useState({ pageNo: 1, limit: 200 });
   const [totalRecords, setTotalRecords] = React.useState(0);
   const [filteredValue, setFilteredValue] = React.useState(columns.map((col) => col.field));
   const [selectedRows, setSelectedRows] = React.useState([]);
+  const [openDetails, setOpenDetails] = React.useState(null);
 
   async function fetchList() {
     try {
@@ -111,6 +125,10 @@ export const PortfolioListView = () => {
   const handleProcessRowUpdateError = React.useCallback((error) => {
     console.log({ children: error.message, severity: 'error' });
   }, []);
+
+  const handleEdit = (params) => {
+    setOpenDetails(params);
+  };
 
   // ******************************data grid handler ends*********************
 
@@ -193,6 +211,12 @@ export const PortfolioListView = () => {
           />
         </Box>
       </Card>
+      <ManagePortfolioRightPanel
+        open={openDetails ? true : false}
+        onClose={() => setOpenDetails(null)}
+        data={openDetails}
+        fetchList={fetchList}
+      />
       {/* </PageLoader> */}
     </PageContainer>
   );
