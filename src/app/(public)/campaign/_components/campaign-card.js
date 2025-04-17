95448ed22d25d139e04ef99d9480abf90f9947f5
codemine24 <codemine24@gmail.com>
2025-04-17 10:25:26 +0600

    campaign card modification done.

--- src/app/(public)/campaign/_components/campaign-card.js
+++ src/app/(public)/campaign/_components/campaign-card.js
@@ -1,6 +1,6 @@
+import React from 'react';
 import { Box, Button, Stack, Typography } from '@mui/material';
 import dayjs from 'dayjs';
-import React from 'react';
 
 import { CustomChip } from '/src/components/core/custom-chip';
 import { Iconify } from '/src/components/iconify/iconify';
@@ -29,14 +29,23 @@ export const CampaignCard = ({ item, fetchList }) => {
           alt={item.Name}
           sx={{
             width: '100%',
-            height: {lg: 350, md: 400, sm: 300, xs: 300 },
+            height: { lg: 350, md: 400, sm: 300, xs: 300 },
             objectFit: 'cover',
             borderRadius: 'calc(2* var(--mui-shape-borderRadius))',
             padding: 1.5,
             objectFit: 'cover',
           }}
         />
-        <Stack direction="column" spacing={4} sx={{ p: 2 }}>
+        <Stack
+          direction="column"
+          justifyContent="space-between" 
+          sx={{
+            p: { xs: 1.5, md: 2 },
+            width: '100%',
+            flexGrow: 1,
+            minHeight: '100%',
+          }}
+        >
           <Box>
             <Typography
               variant="caption"
@@ -75,20 +84,18 @@ export const CampaignCard = ({ item, fetchList }) => {
               View details
             </Button>
           </Box>
-          <Stack direction={{ md: 'row', sm: 'column' }} justifyContent="space-between" gap={1}>
-            <Stack
-              direction="row"
-              alignItems="center"
-              divider={<Iconify icon="pepicons-pencil:line-y" sx={{ color: 'grey.400' }} />}
-            >
-              <CustomChip label={item.campaign_status ?? '-'} color="success" size="small" variant="soft" />
-              <CustomChip
-                label={`${dayjs(item.StartDate).isValid() ? dayjs(item.StartDate).format('DD MMM YYYY') : '-/-'} : ${dayjs(item.EndDate).isValid() ? dayjs(item.EndDate).format('DD MMM YYYY') : '-/-'}`}
-                color="success"
-                size="small"
-                variant="soft"
-              />
-            </Stack>
+          <Stack
+            direction="row"
+            alignItems="center"
+            divider={<Iconify icon="pepicons-pencil:line-y" sx={{ color: 'grey.400' }} />}
+          >
+            <CustomChip label={item.campaign_status ?? '-'} color="success" size="small" variant="soft" />
+            <CustomChip
+              label={`${dayjs(item.StartDate).isValid() ? dayjs(item.StartDate).format('DD MMM YYYY') : '-/-'} : ${dayjs(item.EndDate).isValid() ? dayjs(item.EndDate).format('DD MMM YYYY') : '-/-'}`}
+              color="success"
+              size="small"
+              variant="soft"
+            />
           </Stack>
         </Stack>
       </Stack>
