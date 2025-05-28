8b3502334bf1f6caf2b278c296c16fa8bdcaaa5b
codemine <codemine24@gmail.com>
2025-05-28 13:53:04 +0600

    refactor(home): footer animation section removed

--- src/app/(public)/main-view.js
+++ src/app/(public)/main-view.js
@@ -3,7 +3,6 @@
 import { Box } from '@mui/material';
 
 import { ContentSection } from './components/home/content-section';
-import { FooterAnimation } from './components/home/footer-animation';
 import { HeroSection } from './components/home/hero-section';
 import { PartnerSectionNew } from './components/home/partner-sectionNew';
 import { PortfolioSection } from './components/home/portfolio-section';
@@ -17,7 +16,6 @@ export const MainView = () => {
       <ContentSection />
       <PortfolioSectionNew />
       <PartnerSectionNew isSecondHorizontal={true} />
-      <FooterAnimation />
     </Box>
   );
 };
