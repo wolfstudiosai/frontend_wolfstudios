a00cedd4ee595c421b7e784a76ca65c56a393466
codemine <codemine24@gmail.com>
2025-05-31 13:59:36 +0600

    convert Campaign Assets Delivered by month to weak

--- src/app/(private)/analytics/_components/bar-chart/campaign-assets-deliverd.js
+++ src/app/(private)/analytics/_components/bar-chart/campaign-assets-deliverd.js
@@ -7,25 +7,20 @@ import { NoSsr } from '/src/components/core/no-ssr';
 export function CampaignAssetsDelivered() {
 
     const data = [
-        { name: "Jan", value: 20 },
-        { name: "Feb", value: 25 },
-        { name: "Mar", value: 30 },
-        { name: "Apr", value: 18 },
-        { name: "May", value: 22 },
-        { name: "Jun", value: 28 },
-        { name: "Jul", value: 35 },
-        { name: "Aug", value: 27 },
-        { name: "Sep", value: 22 },
-        { name: "Oct", value: 28 },
-        { name: "Nov", value: 35 },
-        { name: "Dec", value: 27 },
+        { name: "Sun", value: 20 },
+        { name: "Mon", value: 25 },
+        { name: "Tue", value: 30 },
+        { name: "Wed", value: 18 },
+        { name: "Thu", value: 22 },
+        { name: "Fri", value: 28 },
+        { name: "Sat", value: 35 },
     ];
     return (
         <Grid size={{ xs: 12, md: 5 }}>
             <Card sx={{ height: '100%', borderRadius: 0, border: '1px solid var(--mui-palette-divider)' }}>
                 <CardContent>
                     <Typography variant="h6" sx={{ marginBottom: '16px' }}>
-                        Campaign Assets Delivered by Month
+                        Campaign Assets Delivered by Week
                     </Typography>
                     <NoSsr fallback={<Box sx={{ height: 300 }} />}>
                         <ResponsiveContainer width="100%" height={300}>
