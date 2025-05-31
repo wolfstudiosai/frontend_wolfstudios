3ec0e172c3bb93fafb8c3f58e7b46b7a5e4b66df
codemine <codemine24@gmail.com>
2025-05-31 13:00:20 +0600

    top influencers chart added

--- src/app/(private)/analytics/page.js
+++ src/app/(private)/analytics/page.js
@@ -7,19 +7,23 @@ import { Facilities } from './_components/facilities';
 import { PartnerMatrix } from './_components/partner-matrix';
 import { OnboardingByRegion } from './_components/bar-chart/onboarding-by-region';
 import { OnboardingByCreator } from './_components/bar-chart/onboarding-by-creator';
+import { TopInfluencers } from './_components/bar-chart/top-influencers';
 import Grid from '@mui/material/Grid2';
+import AnalyticsBarCharts from './_components/bar-chart/AnalyticsBarCharts';
 
 // export const metadata = { title: `Analytics | Dashboard | ${config.site.name}` };
 
 export default function Page() {
   return (
     <PageContainer>
-      <ContentPerformance />
+      {/* <ContentPerformance />
       <PartnerMatrix />
-      <Facilities />
+      <Facilities /> */}
+      <AnalyticsBarCharts />
       <Grid container spacing={1}>
         <OnboardingByRegion />
         <OnboardingByCreator />
+        <TopInfluencers />
       </Grid>
     </PageContainer>
   );
