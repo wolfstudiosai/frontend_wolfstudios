84504b6fa174e9fb0f94d34b2300b1a7dc19ef7c
codemine <codemine24@gmail.com>
2025-06-17 13:00:24 +0600

    feat(partner): table filter added

--- src/app/(public)/partner/_lib/partner.actions.js
+++ src/app/(public)/partner/_lib/partner.actions.js
@@ -1,14 +1,25 @@
 import { toast } from 'sonner';
 
 import { api } from '/src/utils/api';
-import { getSearchQuery } from '/src/utils/helper';
+import { validateFilters, buildQueryParams, getSearchQuery } from '/src/utils/helper';
 import { uploadFileAsync } from '/src/utils/upload-file';
 
-export const getPartnerListAsync = async (queryParams) => {
+export const getPartnerListAsync = async (queryParams, filters, gate) => {
   try {
-    const searchQuery = getSearchQuery(queryParams);
-    const res = await api.get(`/partner-HQ${searchQuery}`);
-    return { success: true, data: res.data.data.data, totalRecords: res.data.data.count };
+    let apiUrl = `/partner-HQ?page=${queryParams.page}&size=${queryParams.rowsPerPage}`;
+
+    if (filters && filters.length > 0) {
+      // check if all filters are valid
+      const allFiltersValid = validateFilters(filters);
+      if (!allFiltersValid.valid) {
+        toast.error(allFiltersValid.message);
+        return;
+      }
+      const queryParams = buildQueryParams(filters, gate);
+      apiUrl += `&${queryParams}`;
+    }
+    const res = await api.get(apiUrl);
+    return { success: true, data: res.data.data.data, totalRecords: res.data.data.count, metaData: res.data.data.meta };
   } catch (error) {
     toast.error(error.message);
     return { success: false, error: error.response ? error.response.data : 'An unknown error occurred' };
