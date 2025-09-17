aaf725bc1bc716b3175c4b846912ba98b57dcc3c
codemie <codemine24@gmail.com>
2025-09-17 09:28:33 +0600

    build error solved

--- src/app/checkout/page.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { CheckoutPageView } from './_components/checkout-page-view';
-import { config } from '/src/config';
-
-export const metadata = { title: `Checkout | ${config.site.name}` };
-
-export default function Page() {
-  return <CheckoutPageView />;
-}
