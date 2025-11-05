50af69ded20ec3d4c14fb5d1672e848d60262f24
codemine24 <codemine24@gmail.com>
2025-11-05 13:31:18 +0600

    tiptap editor removed.

--- src/components/widgets/modals/modal-1.js
+++ src/components/widgets/modals/modal-1.js
@@ -1,19 +1,12 @@
-import * as React from 'react';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-import Input from '@mui/material/Input';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
-import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
 import { ArrowsOutSimple as ArrowsOutSimpleIcon } from '@phosphor-icons/react/dist/ssr/ArrowsOutSimple';
-import { Image as ImageIcon } from '@phosphor-icons/react/dist/ssr/Image';
-import { Paperclip as PaperclipIcon } from '@phosphor-icons/react/dist/ssr/Paperclip';
 import { X as XIcon } from '@phosphor-icons/react/dist/ssr/X';
+import * as React from 'react';
 
-import { TextEditor } from '/src/components/core/text-editor/text-editor';
 
 export function Modal1() {
   return (
@@ -38,35 +31,6 @@ export function Modal1() {
             <XIcon />
           </IconButton>
         </Stack>
-        <Box sx={{ display: 'flex', flexDirection: 'column', flex: '1 1 auto' }}>
-          <Input fullWidth placeholder="To" />
-          <Divider />
-          <Input fullWidth placeholder="Subject" />
-          <Divider />
-          <Box
-            sx={{ '& .tiptap-root': { border: 'none', borderRadius: 0 }, '& .tiptap-container': { height: '300px' } }}
-          >
-            <TextEditor content="" placeholder="Leave a message" />
-          </Box>
-          <Divider />
-          <Stack direction="row" spacing={3} sx={{ alignItems: 'center', justifyContent: 'space-between', p: 2 }}>
-            <Stack direction="row" spacing={1} sx={{ alignItems: 'center' }}>
-              <Tooltip title="Attach image">
-                <IconButton>
-                  <ImageIcon />
-                </IconButton>
-              </Tooltip>
-              <Tooltip title="Attach file">
-                <IconButton>
-                  <PaperclipIcon />
-                </IconButton>
-              </Tooltip>
-            </Stack>
-            <div>
-              <Button variant="contained">Send</Button>
-            </div>
-          </Stack>
-        </Box>
       </Paper>
     </Box>
   );
