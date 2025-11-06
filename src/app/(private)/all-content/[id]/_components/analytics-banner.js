db95af67e969dd40c74e29b6b3f5a47739ffb514
codemine24 <codemine24@gmail.com>
2025-11-06 16:37:54 +0600

    title change in content hq analytics page.

--- src/app/(private)/all-content/[id]/_components/analytics-banner.js
+++ src/app/(private)/all-content/[id]/_components/analytics-banner.js
@@ -4,36 +4,43 @@ import React from 'react';
 import { Box, Typography } from '@mui/material';
 
 export const AnalyticsBanner = () => {
-    return (
-        <Box
-        sx={{
-          backgroundImage: 'url(/assets/analytics-banner.jpg)',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          height: '220px',
-          width: '100%',
-          overflow: 'hidden',
-          position: 'relative',
-          '&::before': {
-            content: '""',
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-              background: 'linear-gradient(180deg, rgba(0, 0, 0, 0.45) 0%, rgba(0, 0, 0, 0) 100%)',
-          },
-        }}
-        >
-            <Box p={4} height="100%" position="relative" display="flex" flexDirection="column" justifyContent="end" alignItems="flex-start" gap={1}>
-              <Typography variant="h3" color="white" fontWeight={600}>
-                Analytics Dashboard Overview
-            </Typography>
-            <Typography variant="h5">
-                A snapshot of your analytics Campaign, Content, and Portfolio.
-            </Typography>
-            </Box>
-        </Box>
-    );
-}
+  return (
+    <Box
+      sx={{
+        backgroundImage: 'url(/assets/analytics-banner.jpg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        height: '220px',
+        width: '100%',
+        overflow: 'hidden',
+        position: 'relative',
+        '&::before': {
+          content: '""',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          background: 'linear-gradient(90deg, rgba(0, 0, 0, 0.75) 0%, rgba(0, 0, 0, 0) 100%)',
+        },
+      }}
+    >
+      <Box
+        p={4}
+        height="100%"
+        position="relative"
+        display="flex"
+        flexDirection="column"
+        justifyContent="end"
+        alignItems="flex-start"
+        gap={1}
+      >
+        <Typography variant="h3" color="white" fontWeight={600}>
+          Content performance analytics
+        </Typography>
+        <Typography variant="h5">A detailed overview of your performance and analytics. </Typography>
+      </Box>
+    </Box>
+  );
+};
